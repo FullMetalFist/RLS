b51bc51c77b0b27f31e0e0cbbe6d4bf395f216dd
Michael Vilabrera <m.vilabrera@gmail.com>
2015-08-13 14:32:15 -0400

    completed rain-touch with splash emitter

--- RLSSK/SongLyricsSub/RainScene.m
+++ RLSSK/SongLyricsSub/RainScene.m
@@ -15,6 +15,9 @@
 
 @end
 
+static const uint32_t rainCategory = 0x1;
+static const uint32_t bottomCategory = 0x1 << 1;
+
 @implementation RainScene
 
 -(instancetype)initWithSize:(CGSize)size {
@@ -26,6 +29,7 @@
         self.physicsWorld.contactDelegate = self;
         
         
+        [self addBottomEdge];
     }
     return self;
 }
@@ -43,14 +47,46 @@
         CGPoint location = [touch locationInNode:self];
         
         SKSpriteNode *rain = [SKSpriteNode spriteNodeWithImageNamed:@"drop"];
+        rain.name = @"rain";
         rain.physicsBody = [SKPhysicsBody bodyWithCircleOfRadius:rain.frame.size.width];
         rain.physicsBody.affectedByGravity = YES;
         rain.physicsBody.velocity = CGVectorMake(0, -9.8);
+        rain.physicsBody.categoryBitMask = rainCategory;
+        rain.physicsBody.contactTestBitMask = bottomCategory;
         rain.position = location;
         [rain setScale:0.2];
         
         [self addChild:rain];
         // TODO: add category bitmask for connecting drop & emitter splash
+    }
+}
+
+- (void)addSplashAtPosition:(CGPoint)position {
+    SKEmitterNode *splash = [NSKeyedUnarchiver unarchiveObjectWithFile:[[NSBundle mainBundle] pathForResource:@"Splash" ofType:@"sks"]];
+    splash.position = position;
+    [self addChild:splash];
+}
+
+- (void)addBottomEdge {
+    SKNode *bottomNode = [SKNode node];
+    bottomNode.physicsBody = [SKPhysicsBody bodyWithEdgeFromPoint:CGPointMake(0, 5) toPoint:CGPointMake(self.size.width, 5)];
+    bottomNode.physicsBody.categoryBitMask = bottomCategory;
+    [self addChild:bottomNode];
+}
+
+- (void)didBeginContact:(SKPhysicsContact *)contact {
+    SKPhysicsBody *body;
+    if (contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask) {
+        body = contact.bodyA;
+    } else {
+        body = contact.bodyB;
+    }
+    
+    if (body.categoryBitMask == rainCategory) {
+        NSLog(@"Splash!");
+        
+        [body.node removeFromParent];
+        [self addSplashAtPosition:contact.contactPoint];
     }
 }
 
